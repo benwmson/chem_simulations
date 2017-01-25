5b421cd885661b714a1328bfc59c4e3a778d233d
Ben Williamson <benjamin.wmson@gmail.com>
2017-01-24 17:29:13 -0700

    Added matrix class stub constructor / DESTROYER.

---
+++ molecular_dynamics/src/Matrix.cpp
@@ -0,0 +1,16 @@
+
+
+#include "Matrix.h"
+
+namespace std {
+
+Matrix::Matrix() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Matrix::~Matrix() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace storage */
---
+++ molecular_dynamics/src/Matrix.h
@@ -0,0 +1,15 @@
+
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+namespace std {
+
+class Matrix {
+public:
+	Matrix();
+	virtual ~Matrix();
+};
+
+} /* namespace std */
+#endif /* MATRIX_H_ */
