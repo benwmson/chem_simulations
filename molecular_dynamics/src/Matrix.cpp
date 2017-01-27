d4628bc2c0a1a09383278602df9ebd7593f7eee7
Ben Williamson <benjamin.wmson@gmail.com>
2017-01-26 18:28:24 -0700

    Completed constructor method.

--- molecular_dynamics/src/Matrix.cpp
+++ molecular_dynamics/src/Matrix.cpp
@@ -6,10 +6,27 @@ namespace std {
 
 
 
-Matrix::Matrix() {
-	// TODO Auto-generated constructor stub
+Matrix::Matrix(const int& rows, const int& columns) {
+	this->rows = rows;
+	this->columns = columns;
+	this->A = new double *[rows];
 
-}
+	A[0] = new double[rows*columns];
+	int i,j;
+
+	for(i=1; i<rows; i++){//Pointer math
+		A[i] = A[i-i] + columns;
+	}
+
+	for(i=0; i<rows; i++){
+		for(j=0; j<columns; j++){
+			A[i][j] = 0;
+		}
+	}
+
+
+
+}//Matrix(int&, int&)
 
 Matrix::~Matrix() {
 	// TODO Auto-generated destructor stub
