e9e8c931656bf303a16579c6e9b17f2a2d858f25
Ben Williamson <benjamin.wmson@gmail.com>
2017-02-01 18:56:38 -0700

    Created empty atoms class and header.

---
+++ molecular_dynamics/src/Atoms.cpp
@@ -0,0 +1,21 @@
+/*
+ * Atoms.cpp
+ *
+ *  Created on: Feb 1, 2017
+ *      Author: tom
+ */
+
+#include "Atoms.h"
+
+namespace std {
+
+Atoms::Atoms() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Atoms::~Atoms() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ molecular_dynamics/src/Atoms.h
@@ -0,0 +1,20 @@
+/*
+ * Atoms.h
+ *
+ *  Created on: Feb 1, 2017
+ *      Author: tom
+ */
+
+#ifndef ATOMS_H_
+#define ATOMS_H_
+
+namespace std {
+
+class Atoms {
+public:
+	Atoms();
+	virtual ~Atoms();
+};
+
+} /* namespace std */
+#endif /* ATOMS_H_ */
