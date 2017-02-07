ac0cdacf053d820cf29da3512bd0e053e40d1591
Ben Williamson <benjamin.wmson@gmail.com>
2017-02-07 10:34:17 -0700

    Added constructor to atoms class.

--- molecular_dynamics/src/Atoms.cpp
+++ molecular_dynamics/src/Atoms.cpp
@@ -10,6 +10,14 @@ Atoms::Atoms(){
 
 }
 
+Atoms::Atoms(const int& n, const int& d){
+	this->n = n, this->d = d;
+
+	R = Matrix(n,d), V = Matrix(n,d), F = Matrix(n,d), V0 = Matrix(n,d);
+}
+
+
+
 Atoms::~Atoms() {
 	// TODO Auto-generated destructor stub
 }
