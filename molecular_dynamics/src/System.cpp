c563c1bc32b2ad8dbf1c0acea62638fd017a4bb3
Ben Williamson <benjamin.wmson@gmail.com>
2017-02-13 18:38:06 -0700

    Created stub for system class.

---
+++ molecular_dynamics/src/System.cpp
@@ -0,0 +1,21 @@
+/*
+ * System.cpp
+ *
+ *  Created on: Feb 13, 2017
+ *      Author: tom
+ */
+
+#include "System.h"
+
+namespace std {
+
+System::System() {
+	// TODO Auto-generated constructor stub
+
+}
+
+System::~System() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ molecular_dynamics/src/System.h
@@ -0,0 +1,20 @@
+/*
+ * System.h
+ *
+ *  Created on: Feb 13, 2017
+ *      Author: tom
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+namespace std {
+
+class System {
+public:
+	System();
+	virtual ~System();
+};
+
+} /* namespace std */
+#endif /* SYSTEM_H_ */
